6cd5fa8c7dbe396147f95a195bc9677e2d3fcff9
jannikl7 <jannik.hendriksen@gmail.com>
2024-11-23 23:22:09 +0100

    Added a 6th. level

--- src/main/kotlin/Levels.kt
+++ src/main/kotlin/Levels.kt
@@ -39,6 +39,7 @@ class Levels(itemWidth: Double, itemHeight: Double, canvasWidth: Double, canvasH
       }
        levels.add(Level(2, "Level3", level3, Direction.EAST, 25, false, snakeStartX, snakeStartY))
 
+      //------------- Level 4 ----------------
        val level4 = mutableListOf<Obstructing>()
       (3 .. 6).forEach {
          level4.add(SolidObstruction(it*itemWidth, posY = itemHeight*it, itemWidth, itemHeight))
@@ -48,25 +49,56 @@ class Levels(itemWidth: Double, itemHeight: Double, canvasWidth: Double, canvasH
       }
       levels.add(Level(3, "Level4", level4, Direction.EAST, 25, true, snakeStartX, snakeStartY))
 
+      //------------- Level 5 & 6 ----------------
       val level5 = mutableListOf<Obstructing>()
-      val wormHoleGroup = mutableListOf<WormHole>()
+      val level6 = mutableListOf<Obstructing>()
       val wormhole1 = WormHole(itemWidth*3, itemHeight*3, canvasWidth-(itemWidth*3), canvasHeight-(itemHeight*3), itemWidth, itemHeight, color = Color.GREEN )
-      val wormhole2 = WormHole(canvasWidth-(itemWidth*3), itemHeight*3, itemWidth*3, canvasHeight-(itemHeight*3), itemWidth, itemHeight, color = Color.RED )
+      val wormhole2 = WormHole(canvasWidth-(itemWidth*3), itemHeight*3, itemWidth*3, canvasHeight-(itemHeight*3), itemWidth, itemHeight, color = Color.YELLOW )
       level5.add(wormhole1)
       level5.add(wormhole2)
+      level6.add(wormhole1)
+      level6.add(wormhole2)
       for(i in 0..(canvasHeight-itemHeight).toInt() step itemHeight.toInt()) {
-         level5.add(
-            SolidObstruction((canvasWidth/2)-(itemWidth/2), posY = i.toDouble(), itemWidth, itemHeight)
-         )
+         val obstr = SolidObstruction(((canvasWidth/itemWidth)/2)*itemWidth, i.toDouble(), itemWidth, itemHeight)
+         level5.add(obstr)
+         level6.add(obstr)
       }
-      levels.add(Level(4, "Level5", level5, Direction.EAST, 25, true, snakeStartX + (itemWidth*2), snakeStartY))
+
+      val y = (canvasHeight/itemHeight)/2 * itemHeight
+      val xThird = ((canvasWidth/itemWidth)/4).toInt() * itemWidth
+      val xTwoThirds = xThird * 3
+      level6.add(
+         SolidObstruction(xThird-(2*itemWidth), y, itemWidth, itemHeight)
+      )
+      level6.add(
+         SolidObstruction(xThird, y, itemWidth, itemHeight)
+      )
+      level6.add(
+         SolidObstruction(xThird+(itemWidth*2), y, itemWidth, itemHeight)
+      )
+      level6.add(
+         SolidObstruction(xTwoThirds-(2*itemWidth), y, itemWidth, itemHeight)
+      )
+      level6.add(
+         SolidObstruction(xTwoThirds, y, itemWidth, itemHeight)
+      )
+      level6.add(
+         SolidObstruction(xTwoThirds+(itemWidth*2), y, itemWidth, itemHeight)
+      )
+
+      levels.add(Level(4, "Level5", level5, Direction.EAST, 25, false, snakeStartX + (itemWidth*2), snakeStartY))
+      levels.add(Level(4, "Level6", level6, Direction.EAST, 25, false, snakeStartX + (itemWidth*2), (canvasHeight/itemHeight)*3))
+
+
+
+      levels.lastOrNull()?.let { it.finalLevel = true }
    }
 
    fun getLevel(idx: Int): Level {
       return levels[idx-1].copy(board = levels[idx-1].board.toMutableList())
    }
 
-   data class Level(val idx: Int, val name: String, val board: MutableList<Obstructing>, val startDirection: Direction, val goalPoints: Int, val finalLevel: Boolean, val startPosX: Double, val startPosY: Double)
+   data class Level(val idx: Int, val name: String, val board: MutableList<Obstructing>, val startDirection: Direction, val goalPoints: Int, var finalLevel: Boolean, val startPosX: Double, val startPosY: Double)
 
    companion object {
       val levels: MutableList<Level> = mutableListOf()
@@ -75,8 +107,8 @@ class Levels(itemWidth: Double, itemHeight: Double, canvasWidth: Double, canvasH
       }
 
       fun getFirstLevel(): Levels.Level? {
-         //return levels.firstOrNull()
-         return levels.last()
+        // return levels.firstOrNull()
+         return levels.lastOrNull()
       }
    }
 }
